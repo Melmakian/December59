fdad3c61a2d07d9d2116716b803ae5abdc0d0809
Alf <siidousdart@gmail.com>
2022-12-08 20:37:45 +0500

    add all

---
+++ src/December59.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class December59 {
+    public class JavaRush implements Serializable{
+        public List<User> users = new ArrayList();
+    }
+}
